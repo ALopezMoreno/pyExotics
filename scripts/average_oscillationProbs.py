a5dcc2a33fe7839ecf99c097b63401fae036aa38
ALopezMoreno <andres.lopez_moreno@kcl.ac.uk>
2023-02-17 08:06:33 +0000

    fixed cluster code

--- scripts/average_oscillationProbs.py
+++ scripts/average_oscillationProbs.py
@@ -7,7 +7,7 @@ def average_files(files):
     completed = 0
     for file in files:
         data.append(np.loadtxt(file))
-	completed += 1
+        completed += 1
         print("loaded " + str(completed) + " files out of " + str(len(files)))
     average = np.mean(data, axis=0)
     return average
